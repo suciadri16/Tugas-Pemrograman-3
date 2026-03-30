2a0d1ab3a314df3ab71f74620fd27972981cc5b9
Suci Adri Avrillia-I.2410307 <suci.adriavrillia16@gmail.com>
2026-03-30 22:47:21 +0700

    Add files via upload

---
+++ latihan.py
@@ -0,0 +1,16 @@
+# Input
+n1 = int(input("Masukkan Nilai pertama: "))
+n2 = int(input("Masukkan Nilai kedua  : "))
+n3 = int(input("Masukkan Nilai ketiga : "))
+
+# Proses
+jumlah = n1 + n2 + n3
+kurang = n1 - n2 - n3
+kali = n1 * n2 * n3
+bagi = n1 / n2 / n3
+
+# Output
+print("Jumlah dari n1 + n2 + n3 =", jumlah)
+print("kurang dari n1 - n2 - n3 =", kurang)
+print("kali   dari n1 * n2 * n3 =", kali)
+print("bagi   dari n1 / n2 / n3 =", bagi)
